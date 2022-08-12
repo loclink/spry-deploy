5dc0039f87a3e74bfcf919734c0b36885e98a51f
loclink <loclink@163.com>
2022-08-12 14:47:05 +0800

    🧪 test: 测试

--- src/middleware/webhooks.middleware.js
+++ src/middleware/webhooks.middleware.js
@@ -1,45 +1,43 @@
-const crypto = require('crypto')
+const crypto = require('crypto');
 
-const config = require('../app/config')
+const config = require('../app/config');
 
-const { errorTypes } = require('../error/error-types')
+const { errorTypes } = require('../error/error-types');
 
 class WebHooksMiddleware {
   // 校验gitee的参数
   async authWebhooksGitee(ctx, next) {
-    const repositoryName = ctx.request.body.project?.name
-    if (!repositoryName) ctx.emitError(errorTypes.MISSING_PARAMETER)
-    const { password } = ctx.request.body
+    const repositoryName = ctx.request.body.project?.name;
+    if (!repositoryName) ctx.emitError(errorTypes.MISSING_PARAMETER);
+    const { password } = ctx.request.body;
 
     if (config.password !== password) {
-      ctx.emitError(errorTypes.TOKEN_CHECK_FAILED)
+      ctx.emitError(errorTypes.TOKEN_CHECK_FAILED);
     }
 
-    ctx.request.body._repositoryName = repositoryName
-    await next()
+    ctx.request.body._repositoryName = repositoryName;
+    await next();
   }
 
   // 校验github的参数
   async authWebhooksGithub(ctx, next) {
-    const sha1 = ctx.request.headers['x-hub-signature']
-    const repositoryName = ctx.request.body.repository?.name
+    const sha1 = ctx.request.headers['x-hub-signature'];
+    const repositoryName = ctx.request.body.repository?.name;
 
-    if (!sha1) ctx.emitError(errorTypes.TOKEN_CHECK_FAILED)
-    if (!repositoryName) ctx.emitError(errorTypes.MISSING_PARAMETER)
+    if (!sha1) ctx.emitError(errorTypes.TOKEN_CHECK_FAILED);
+    if (!repositoryName) ctx.emitError(errorTypes.MISSING_PARAMETER);
 
-    const token = `sha1=${crypto
-      .createHmac('sha1', config.password)
-      .update(JSON.stringify(ctx.request.body))
-      .digest('hex')}`
+    const token = `sha1=${crypto.createHmac('sha1', config.password).update(JSON.stringify(ctx.request.body)).digest('hex')}`;
 
     if (token !== sha1) {
-      ctx.emitError(errorTypes.TOKEN_CHECK_FAILED)
+      ctx.emitError(errorTypes.TOKEN_CHECK_FAILED);
     }
 
-    ctx.request.body._repositoryName = repositoryName
+    ctx.request.body._repositoryName = repositoryName;
 
-    await next()
+    console.log(ctx.request.body);
+    await next();
   }
 }
 
-module.exports = new WebHooksMiddleware()
+module.exports = new WebHooksMiddleware();
