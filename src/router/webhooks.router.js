58b367bd112bc22559dfd944018a4e98a04d5d39
loclink <loclink@163.com>
2022-08-12 14:51:51 +0800

    🧪 test: 测试

--- src/router/webhooks.router.js
+++ src/router/webhooks.router.js
@@ -1,11 +1,12 @@
-const Router = require('koa-router')
-const {
-  authWebhooksGitee,
-  authWebhooksGithub
-} = require('../middleware/webhooks.middleware')
-const { handleWebHooks } = require('../controller/webhooks.controller')
+const Router = require('koa-router');
+const { authWebhooksGitee, authWebhooksGithub } = require('../middleware/webhooks.middleware');
+const { handleWebHooks } = require('../controller/webhooks.controller');
 
-const WebHooksRouter = new Router({ prefix: '/webhooks' })
-WebHooksRouter.post('/gitee', authWebhooksGitee, handleWebHooks)
-WebHooksRouter.post('/github', authWebhooksGithub, handleWebHooks)
-module.exports = WebHooksRouter
+const WebHooksRouter = new Router({ prefix: '/webhooks' });
+WebHooksRouter.post('/gitee', authWebhooksGitee, handleWebHooks);
+WebHooksRouter.post('/github', authWebhooksGithub, handleWebHooks);
+WebHooksRouter.get('/', async (ctx,next) => {
+  console.log('aaaaaaaa');
+  await next()
+});
+module.exports = WebHooksRouter;
